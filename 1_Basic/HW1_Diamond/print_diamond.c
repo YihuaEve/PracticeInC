cc73a56d687f96f2c2e7515cd1b91982f6ab3d10
YihuaEve <yihua.eve@gmail.com>
2024-10-07 10:35:11 +0800

    refactor: limit input number range and set input as output lines

--- 1_Basic/HW1_Diamond/print_diamond.c
+++ 1_Basic/HW1_Diamond/print_diamond.c
@@ -1,29 +1,23 @@
 /**
 語法第一個作業：讓我輸入一個數字，輸出數字行數的菱形
 
-input: 0, output: 
+input限制: odd number, [1,19]
 
 input: 1, output: 
 *
 
-input: 2, output:
+input: 3, output:
  *
 ***
  *
  
-input: 3, output:
-  *
- ***
-*****
- ***
-  *
- 
 **/
 
 #include <stdio.h>
+#include <stdlib.h>
 
 // function: print a line with white spaces and marks
-int print_mark_line(int white_space_count, int mark_count){
+int print_marked_line(int white_space_count, int mark_count){
     for (int i = 0; i < white_space_count; i++, printf(" "));
     for (int i = 0; i < mark_count; i++, printf("*"));
     printf("\n");
@@ -33,29 +27,24 @@ int print_mark_line(int white_space_count, int mark_count){
 int main(void){
     int number;
 
-    // wait for user input
-    printf("Please enter a positive integer: \n");
+    // 1. Wait for user input
+    printf("Please enter a positive odd integer between 1 and 19: \n");
     scanf("%d", &number);
-    printf("The entered number: %d\n", number);
+    printf("The entered number: %d. \n", number);
     
-    // check: Invalid number
-    if(number < 0){
-        printf("Invalid number: %d. Please enter a positive integer. \n", number);
+    // 2. Check: Invalid number
+    if(number < 1 || number > 19 || !(number & 1)){
+        printf("[ERROR] Illegal input: %d.\n", number);
+        printf("Please enter a positive odd integer between 1 and 19. \n");
         return 0;
     }
     
-    printf("Output Diamond: \n");    
-    // print upper part of diamond
+    // 3. Print diamond pattern
+    printf("Output: \n");
     for (int i = 0; i < number; i++){
-        int white_space_count = number - i - 1;
-        int mark_count = 2 * i + 1;
-        print_mark_line(white_space_count, mark_count);
-    }
-    // print upper part of diamond
-    for (int i = 0; i < (number - 1); i++){
-        int white_space_count = i + 1;
-        int mark_count = 2 * (number - 2 - i) + 1;
-        print_mark_line(white_space_count, mark_count);
+        int white_space_count = abs((number / 2) - i);
+        int mark_count = number - (2 * white_space_count);
+        print_marked_line(white_space_count, mark_count);
     }
 
     return 0;
